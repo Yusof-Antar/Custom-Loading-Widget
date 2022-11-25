b1b7e8acd6c13c2d0fb3eb229c69151f5dd0c31f
yusof_antar <youssof.antar@gmail.com>
2022-11-25 11:09:21 +0200

    final code done

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,20 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: Center(
-          child: Row(
-            children: const [],
+          child: SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ...List.generate(
+                  30,
+                  (index) => CustomLoadingWidget(
+                    index: index + 1,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -26,15 +38,60 @@ class MyApp extends StatelessWidget {
 }
 
 class CustomLoadingWidget extends StatefulWidget {
-  const CustomLoadingWidget({super.key});
+  final int index;
+  const CustomLoadingWidget({super.key, required this.index});
 
   @override
   State<CustomLoadingWidget> createState() => _CustomLoadingWidgetState();
 }
 
-class _CustomLoadingWidgetState extends State<CustomLoadingWidget> {
+class _CustomLoadingWidgetState extends State<CustomLoadingWidget> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 800,
+      ),
+    );
+    _scaleAnimation = Tween<double>(begin: 2.0, end: 50.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+
+    super.initState();
+    Future.delayed(
+      Duration(milliseconds: widget.index * 180),
+      () {
+        _animationController.forward();
+      },
+    );
+
+    _animationController.addListener(() {
+      if (_animationController.isCompleted) {
+        _animationController.reverse();
+      } else if (_animationController.isDismissed) {
+        _animationController.forward();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnimatedBuilder(
+        animation: _scaleAnimation,
+        builder: (context, child) {
+          return Container(
+            height: _scaleAnimation.value,
+            width: 5,
+            margin: const EdgeInsets.all(1),
+            decoration: BoxDecoration(
+              color: Colors.cyan,
+              borderRadius: BorderRadius.circular(5),
+            ),
+          );
+        });
   }
 }
